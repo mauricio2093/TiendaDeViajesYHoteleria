60dd457dba7a0e5ce4ecbedf1bf9bc44e8b8c8c9
mauricio2093 <mauricio2093@gmail.com>
2021-09-27 00:34:28 -0500

    SE suve la animacion de js de la parte del scroll y cambio de tema

--- ProyectoCiclo3MinTic/TiendaDeViajesYHoteleria/assets/js/main.js
+++ ProyectoCiclo3MinTic/TiendaDeViajesYHoteleria/assets/js/main.js
@@ -1,23 +1,102 @@
-/*==================== SHOW MENU ====================*/
+/*==================== mostrar menu ====================*/
 
-/*===== MENU SHOW =====*/
+/*===== se muestra el menu =====*/
 
-/*===== MENU HIDDEN =====*/
+/*===== se esconde el menu=====*/
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*==================== remover el menu  desplegable====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/
+/*==================== cambiar el backgroun del header ====================*/
 
 /*==================== SWIPER DISCOVER ====================*/
 
 
 /*==================== VIDEO ====================*/
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== mostrar SCROLL arriba ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // Cuando el desplazamiento es superior a la altura de la ventana gráfica 200, agregue la clase show-scroll a la etiqueta a con la clase scroll-top
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== SCROLL SECCION DE ENLACE ACTIVO====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+/*==================== SCROLL animacion ====================*/
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
 
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+sr.reveal(`.home__data, .home__social-link, .home__info,
+            .discover__container,
+            .experience__data, .experience__overlay,
+            .place__card,
+            .sponsor__content,
+            .footer__data, .footer__rights`,{
+    origin: 'top',
+    interval: 100,
+})
 
-/*==================== SCROLL REVEAL ANIMATION ====================*/
+sr.reveal(`.about__data, 
+            .video__description,
+            .subscribe__description`,{
+    origin: 'left',
+})
 
-/*==================== DARK LIGHT THEME ====================*/ 
+sr.reveal(`.about__img-overlay, 
+            .video__content,
+            .subscribe__form`,{
+    origin: 'right',
+    interval: 100,
+})
+
+/*==================== TEMA OSCURO Y CLARO ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Tema seleccionado previamente (si está seleccionada por el usuario)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//  Obtenemos el tema actual que tiene la interfaz validando la clase dark-theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// Validamos si el usuario eligió previamente un tema
+if (selectedTheme) {
+  // Si se cumple la validación, preguntamos cuál fue el problema para saber si activamos o desactivamos el oscuro
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activar / desactivar el tema manualmente con el botón
+themeButton.addEventListener('click', () => {
+    // Agregar o eliminar el tema oscuro / icono
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Guardamos el tema y el icono actual que eligió el usuario
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
